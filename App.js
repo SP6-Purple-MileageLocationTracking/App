54061bef87176c86b6955c9b23669d19683f63ce
Zer0Tym3 <45374225+Zer0Tym3@users.noreply.github.com>
2024-03-13 19:45:21 -0400

    File Upload

---
+++ App.js
@@ -0,0 +1,44 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+/* eslint-disable react/react-in-jsx-scope */
+import { StatusBar } from 'expo-status-bar';
+import {
+    StyleSheet, Button, View, SafeAreaView,
+    Text, Alert
+} from 'react-native';
+import React from 'react';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+          <Text style={{fontWeight: "bold"}}>Time-Mileage App</Text>
+          <Text>Team SP6 - Purple: Mason Sherrill, Alex Nguyen, Brian Nghiem, Shaun Teague, Zachary Morning</Text>
+          <Text>Testing 1...2...3...</Text>
+          <Text style={{
+              fontWeight: "800"}}>Welp, It does seem to work...</Text>
+
+          <Button
+
+              // Some properties given to Button 
+              title="Test Button"
+              onPress={() => Alert.alert(
+                  'Thank you for Testing')}
+          /> 
+          <Text>This Code is in TypeScript</Text>
+          
+
+
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
