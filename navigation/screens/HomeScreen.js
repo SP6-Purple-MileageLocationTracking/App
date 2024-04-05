f413448ded9fd6037bd5ba7c899a138f7e16d9b6
IcedTeague <shaunteague06@outlook.com>
2024-04-05 13:37:14 -0400

    Update HomeScreen.js
    added buttons and places for data to go on the home screen

--- navigation/screens/HomeScreen.js
+++ navigation/screens/HomeScreen.js
@@ -3,28 +3,110 @@
 import { StatusBar } from 'expo-status-bar';
 import {
     StyleSheet, Button, View, SafeAreaView,
-    Text, Alert, TouchableOpacity,
+    Text, Alert, TouchableOpacity, 
 } from 'react-native';
-import React from 'react';
+import React, {useState} from 'react';
 import Ionicons from 'react-native-vector-icons/Ionicons'
+
 
 
 export default function HomeScreen({navigation}) {
 
+    const [tripStarted, setTripStarted] = useState(false)
+    const [tripPlay, setTripPlay] = useState(false)
+
+    const [date, setDate] = useState('')
+    const [startLoc, setStartLoc] = useState('')
+    const [time, setTime] = useState('')
+    const [dis, setDis] = useState(0)
+
     const onPressStart = () => {
+        //put start location code here -S
+        setDate('3/6/24');
+        setStartLoc('Tampa, FL')
+        setTime('6:23')
+        setDis(472)
+        //
         console.log('Start Pressed')
+        setTripPlay(true);
+        setTripStarted(true);
+    };
+
+    const onPressPause = () => {
+        //Stop keeping track of time/location when this button is pressed -S
+        setTripPlay(false);
+        console.log('Pause Pressed')
+    };
+
+    const onPressPlay = () => {
+        //Resume keeping track of time/location when this button is pressed -S
+        setTripPlay(true);
+        console.log('Play Pressed')
+    };
+
+    const onPressEnd = () => {
+        //the end button will send trip data to the database
+        //put that code here -S
+        console.log('End Pressed')
+        setTripPlay(false);
+        setTripStarted(false);
     };
 
 
     return(
         <View style={styles.container}>
             <View style={styles.currentTrip}>
-                <Ionicons name="car-outline" size={60} margin={20}  color="#f2d15f"></Ionicons>
+            {tripStarted && (
+                    <SafeAreaView style={{flexDirection:'column', marginTop: 20}}>
+                        <Text style={styles.headerText}>{date}</Text>
+                        <Text style={styles.headerText}>Left From: {startLoc}</Text>
+
+                        <SafeAreaView style={{marginLeft: 35, marginTop: 25, marginBottom: 30}}>
+                            <SafeAreaView style={{flexDirection:'row'}}>
+                                <Ionicons name="time-outline" size={30} color="#f2d15f" />
+                                <Text style={styles.infoText}>Time:</Text>
+                                <Text style={styles.numText}>{time}</Text>
+                            </SafeAreaView>
+
+                            <SafeAreaView style={{flexDirection:'row',marginTop:15,}}>
+                                <Ionicons name="car-outline" size={30} color="#f2d15f" />
+                                <Text style={styles.infoText}>Distance:</Text>
+                                <Text style={styles.numText}>{dis}</Text>
+                            </SafeAreaView>
+                        </SafeAreaView>
+
+                    </SafeAreaView>
+            )}
             </View>
+            <View style={styles.buttonContainer}>
+                {!tripStarted && (
                 <TouchableOpacity onPress={onPressStart} style={styles.startTrip}>
                     <Ionicons name="add-outline" size={40} color="#f2d15f" />
                     <Text style={styles.startText}>Start Trip</Text>
                 </TouchableOpacity>
+                )}
+                {tripStarted &&(
+                    <View style={styles.buttonContainer}>
+                    {tripPlay &&(
+                     <TouchableOpacity onPress={onPressPause} style={styles.tripButtons}>
+                        <Ionicons name="pause-outline" size={40} color="#f2d15f" />
+                        <Text style={styles.startText}>Pause Trip</Text>
+                    </TouchableOpacity>
+                    )}
+                    {!tripPlay &&(
+                     <TouchableOpacity onPress={onPressPlay} style={styles.tripButtons}>
+                        <Ionicons name="play-outline" size={40} color="#f2d15f" />
+                        <Text style={styles.startText}>Play Trip</Text>
+                    </TouchableOpacity>
+                    )}
+
+                    <TouchableOpacity onPress={onPressEnd} style={styles.tripButtons}>
+                        <Ionicons name="stop-circle-outline" size={40} color="#f2d15f" />
+                        <Text style={styles.startText}>End Trip</Text>
+                    </TouchableOpacity>
+                    </View>
+                )}
+            </View>
         </View>
     );
 }
@@ -39,7 +121,7 @@ const styles = StyleSheet.create({
     currentTrip: {
         width: '90%',
         flexDirection: 'row',
-        height: 300,
+        height: '30%',
         backgroundColor: '#211D26',
         borderRadius: 20,
     },
@@ -60,6 +142,43 @@ const styles = StyleSheet.create({
         fontSize: 20,
         color: "#f2d15f",
         marginLeft: 5,
+    },
+    buttonContainer: {
+        flexDirection:'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        justifyContent: 'center',
+        width: '100%'
+    },
+    tripButtons: {
+        width: "45%",
+        backgroundColor: "#201d25",
+        color: "#f2d15f",
+        borderRadius: 25,
+        flexDirection: 'row',
+        justifyContent: "center",
+        alignContent:"center",
+        alignItems: 'center',
+        padding: 20,
+        marginTop: 20,
+        marginBottom: 10,
+        marginHorizontal: 5
+    },
+    headerText: {
+        fontSize: 25,
+        color: "#f2d15f",
+        marginHorizontal: 30,
+        marginVertical: 5,
+    },
+
+    infoText: {
+        fontSize: 20,
+        color: "#f2d15f",
+        marginHorizontal: 10,
+    },
+    numText: {
+        fontSize: 20,
+        color: "#FFFFFF",
     },
   });
   
